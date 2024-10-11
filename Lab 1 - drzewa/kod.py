7187b625fe674fb57b69f39fd0a55c62a92c4a9f
DragonSilverWind <julka.samp.20@wp.pl>
2024-10-11 15:42:51 +0200

    test

--- Lab 1 - drzewa/kod.py
+++ Lab 1 - drzewa/kod.py
@@ -4,13 +4,11 @@ import treelib
 # entropy calculation
 def entropia(lista):
     e = 0
-    suma = 0
-    for i in lista:
-        suma += i
-    sum2 = sum(lista)
-    print(sum2)
+    suma = sum(lista)
     for i in lista:
         e -= (i/suma) * math.log(i/suma, 2)
     return e
 
 print(entropia([2, 4]))
+
+
