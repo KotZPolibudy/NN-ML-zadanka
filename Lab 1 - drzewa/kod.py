50949b6bb77af97f849960ba94ff925a9e6c47a5
KotZPolibudy <wojtek27wojtek@gmail.com>
2024-10-11 15:11:44 +0200

    Check interpretera Julka

--- Lab 1 - drzewa/kod.py
+++ Lab 1 - drzewa/kod.py
@@ -4,11 +4,13 @@ import treelib
 # entropy calculation
 def entropia(lista):
     e = 0
-    sum = 0
+    suma = 0
     for i in lista:
-        sum += i
+        suma += i
+    sum2 = sum(lista)
+    print(sum2)
     for i in lista:
-        e -= (i/sum) * math.log(i/sum, 2)
+        e -= (i/suma) * math.log(i/suma, 2)
     return e
 
 print(entropia([2, 4]))
